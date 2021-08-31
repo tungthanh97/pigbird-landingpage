d94bf233478d015dd12e62a84ce255496951ff6f
ichigofan <tung020197@gmail.com>
2021-08-31 14:52:49 +0700

    first version

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+var win = $(window);
+function addAnimation(element, animation) {
+  const allElements = $(element);
+  //   allElements.each(function (i, el) {
+  //     var element = $(el);
+  //     if (element.visible(true)) {
+  //       element.addClass('already-visible');
+  //     }
+  //   });
+
+  win.scroll(function (event) {
+    allElements.each(function (i, el) {
+      var element = $(el);
+      if (element.visible(true)) {
+        element.addClass(animation);
+      }
+    });
+  });
+}
+
+addAnimation('.moveInRight', 'text-moveInRight');
+addAnimation('.moveInLeft', 'text-moveInLeft');
+addAnimation('.avatar-box', 'fadeIn');
+addAnimation('img', 'fadeIn');
