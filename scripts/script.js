aecefbbdf733b82669fa14ad78309f5e34c85b6a
ichigofan <tung020197@gmail.com>
2021-09-01 20:01:05 +0700

    Add hover zoom animation

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,6 @@
 var win = $(window);
 function addAnimation(element, animation) {
   const allElements = $(element);
-  //   allElements.each(function (i, el) {
-  //     var element = $(el);
-  //     if (element.visible(true)) {
-  //       element.addClass('already-visible');
-  //     }
-  //   });
-
   win.scroll(function (event) {
     allElements.each(function (i, el) {
       var element = $(el);
@@ -20,7 +13,7 @@ function addAnimation(element, animation) {
   });
 }
 
-addAnimation('.moveInRight', 'text-moveInRight');
-addAnimation('.moveInLeft', 'text-moveInLeft');
-addAnimation('.avatar-box', 'fadeIn');
-addAnimation('img', 'fadeIn');
+// addAnimation('.moveInRight', 'text-moveInRight');
+// addAnimation('.moveInLeft', 'text-moveInLeft');
+// addAnimation('.avatar-box', 'fadeIn');
+// addAnimation('img', 'fadeIn');
